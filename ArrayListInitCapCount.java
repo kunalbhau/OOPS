88bf862d2785873d23ee24b25ae4b2af8c371492
kunal kale <84256660+kunalbhau@users.noreply.github.com>
2025-06-18 19:09:09 +0530

    Add files via upload

---
+++ ArrayListInitCapCount.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+public class ArrayListInitCapCount
+ {
+ public static void main(String args[])
+  {
+  ArrayList<Integer > Number = new ArrayList<>(5);
+  
+  Number.add(10);
+  Number.add(20);
+  Number.add(30);
+  Number.add(40);
+  Number.add(50);
+  
+  System.out.println(Number);
+  
+  }
+  }
---
+++ NoArg.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+public class NoArg
+
+{
+	public static void main(String args[])
+	{
+		ArrayList<String> Name = new ArrayList <> ();
+		
+		Name.add("Rohit");
+		Name.add("Pawar");
+		
+		System.out.println(Name);
+	}
+}
